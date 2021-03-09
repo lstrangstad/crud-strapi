f93cd5497a2562966bebdf679d3d5b912ec26f6f
Linus <linusstrangstad@gmail.com>
2021-03-09 20:28:30 +0100

    Feat: Fetch products

---
+++ src/pages/Products.js
@@ -0,0 +1,49 @@
+import { useContext, useState, useEffect } from "react";
+import { useHistory, Link } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
+import useAxios from "../utils/useAxios";
+import { PRODUCTS_PATH } from "../utils/constants";
+import Item from "../components/Item";
+
+const Products = () => {
+  const [auth] = useContext(AuthContext);
+  const [products, setProducts] = useState(null);
+  const http = useAxios();
+  const history = useHistory();
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const response = await http.get(PRODUCTS_PATH);
+        console.log(response);
+        setProducts(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProducts();
+  }, []);
+
+  if (!auth) {
+    history.push("/login");
+  }
+
+  if (!products) {
+    return <p>loading...</p>;
+  }
+
+  return (
+    <>
+      <h1>Products</h1>
+      {products?.map((product) => {
+        return (
+          <Link key={product.id} to={`/edit/${product.id}`}>
+            <Item {...product} />;
+          </Link>
+        );
+      })}
+    </>
+  );
+};
+
+export default Products;
