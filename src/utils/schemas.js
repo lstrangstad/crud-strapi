3a9bf1d30cb4b5c2a9cdb645c6f8f1c16f69f877
Linus <linusstrangstad@gmail.com>
2021-03-09 20:28:52 +0100

    Feat: productSchema

--- src/utils/schemas.js
+++ src/utils/schemas.js
@@ -4,3 +4,10 @@ export const loginSchema = yup.object().shape({
   identifier: yup.string().required("Please enter your username"),
   password: yup.string().required("Please enter your password"),
 });
+
+export const productSchema = yup.object().shape({
+  title: yup.string().required("Please provide a title"),
+  price: yup.number().required("Please provide a price"),
+  description: yup.string().required("Please provide a description"),
+  image_url: yup.string().required("Please provide an image url"),
+});
