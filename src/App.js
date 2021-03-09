f168fb81d17205bf2631e1f2e5fee355b0608754
Linus <linusstrangstad@gmail.com>
2021-03-09 20:26:54 +0100

    Feat: Routing products, edit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import Login from "./pages/Login";
 import HomePage from "./pages/HomePage";
+import Products from "./pages/Products";
+import Nav from "./components/Nav";
+import EditProducts from "./pages/EditProducts";
 
 const App = () => {
   return (
     <AuthProvider>
       <Router>
+        <Nav />
         <Switch>
           <Route path="/" exact component={HomePage} />
           <Route path="/login" component={Login} />
+          <Route path="/products" component={Products} />
+          <Route path="/edit/:id" component={EditProducts} />
         </Switch>
       </Router>
     </AuthProvider>
